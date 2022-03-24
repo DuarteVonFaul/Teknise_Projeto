8e3e53dabd0efb4bf3d60fe618d16c29bc94a05d
DuarteVonFaul <50261190+DuarteVonFaul@users.noreply.github.com>
2022-03-24 09:44:25 -0300

    Ultima Aula WebBasic

--- WebBasic/js/app.js
+++ WebBasic/js/app.js
@@ -118,4 +118,65 @@ function preRender() {
   function showCard(card) {
     card.style.display = 'flex'
   }
+
+
+  const modalTemplate = `<div class="modal">
+                            <button class="fechar">x</button>
+                            <div class="profilePictureModal">
+                              <img class="image" name = "imageProfile" src = "__DEV_IMAGE__" alt="deselvonvedor"/>
+                            </div>
+                            <div class = "profileDescriptionModal">
+                              <div style="display: flex; flex-direction: colunm">
+                                <h2 class="cardTitle" name = "devname">__DEV_NAME__</h2>
+                                <span name="age">__DEV_AGE__</span>
+                                <span name="description">__DEV_DESCRIPTION__</span>
+                                <span name="description">__DEV_DESCRIPTION_ABILITY__</span>
+                                <h3>Contato</h3>
+                                <span name="mail">Email: <a href="#">__DEV_MAIL__</a></small>
+                                <span name="git">GitHub: <a href="#">__DEV_GIT__</a>5</span>
+                                <span name="phone">Telefone:__DEV_PHONE__</span>
+                              </div>
+                              <div class="languages">
+                                <h3>Linguagens</h3>
+                                __DEV_LANGUAGES__
+                              </div>
+                            </div>
+                          </div>`;
+
+
+
+Array.from(document.querySelectorAll('.card')).forEach(card => {
+  card.addEventListener('click', () => iniciaModal('modalProfile', event.currentTarget.id));
+});
+
+
+function iniciaModal(modalID,cardID){
+  fillModal(getUserInfo(cardID));
+  const modal = document.getElementById(modalID);
+  if(modal){
+    modal.classList.add('mostrar');
+    modal.addEventListener("click",(e)=>{
+
+      if(e.target.id == modalID || e.target.className == "fechar"){
+        modal.classList.remove('mostrar');
+      }
+
+    })
+  }
+}
+
+function getUserInfo(id){
+  let cardUser = document.getElementById(id);
+  let userData = {};
+
+  if(cardUser){
+    userData = ['age', 'mail', 'phone', 'github', 'username', 'description', 'descriptionAbility'].
+                                                reduce((acc,name) => {
+
+                                                  acc[name] = getTextContentByName(cardUser,name);
+                                                  return acc;
+
+                                                }, {});
+  }
+}
   
